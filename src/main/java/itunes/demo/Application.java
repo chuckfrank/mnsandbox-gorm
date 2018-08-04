20283795d585bf7ca51ff2d36d472db6cdd0991e
Charles Frank <cpfrank@localhost.localdomain>
2018-08-04 14:49:23 -0700

    Initial import of gorm demo.

---
+++ src/main/java/itunes/demo/Application.java
@@ -0,0 +1,10 @@
+package itunes.demo;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
